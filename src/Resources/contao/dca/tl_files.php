b98562d914f435c5d95a7b949386d9837136c887
o.magdi <o.magdi@eikona-media.de>
2024-06-04 14:54:00 +0200

    refactor services tl_files

--- src/Resources/contao/dca/tl_files.php
+++ src/Resources/contao/dca/tl_files.php
@@ -2,6 +2,8 @@
 
 use Doctrine\DBAL\Types\Types;
 
-$GLOBALS['TL_DCA']['tl_files']['fields']['active_file'] = [
+$GLOBALS['TL_DCA']['tl_files']['fields'] = array_merge($GLOBALS['TL_DCA']['tl_files']['fields'],[
+    'active_file'=>[
     'sql' => ['type' => Types::BOOLEAN, 'default' => false],
-];
+        ]
+]);
