49d2f39a06ac5d32def8693896a5229ff12af7f7
o.magdi <o.magdi@eikona-media.de>
2024-06-04 13:43:47 +0200

    refactor namespace

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,14 +6,14 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use EikonaMedia\Contao\ActiveFilesUsageBundle\ContaoActiveFilesUsageBundle;
+use EikonaMedia\Contao\ActiveFilesUsageBundle\EikonaMediaContaoActiveFilesUsageBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(ContaoActiveFilesUsageBundle::class)
+            BundleConfig::create(EikonaMediaContaoActiveFilesUsageBundle::class)
                 ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
